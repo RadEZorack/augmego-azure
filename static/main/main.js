4b0b980884f3427d838b4a646b8418b7d9ed7679
Travis Miller <radezorack@gmail.com>
2023-09-23 13:37:19 -0400

    added orbit controls

--- static/main/main.js
+++ static/main/main.js
@@ -1,11 +1,19 @@
 import * as THREE from '../three/three.module.min.js';
+import { OrbitControls } from '../three/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// const camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set( 0, 20, 100 );
+controls.update();
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -19,7 +27,10 @@ function animate() {
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
 	renderer.render( scene, camera );
 }
 animate();
