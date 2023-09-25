73f8066dd6c38dc528706d53363762597319e936
Travis Miller <radezorack@gmail.com>
2023-09-25 17:12:46 -0400

    Basic terrain and player

--- static/main/main.js
+++ static/main/main.js
@@ -1,8 +1,8 @@
 import * as THREE from '../three/three.module.min.js';
 import { OrbitControls } from '../three/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+export const objectScene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 // const camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -15,22 +15,26 @@ const controls = new OrbitControls( camera, renderer.domElement );
 camera.position.set( 0, 20, 100 );
 controls.update();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// objectScene.add( cube );
 
 camera.position.z = 5;
+
+const light = new THREE.HemisphereLight( 0xffffff, 0x888888, 3 );
+light.position.set( 0, 1, 0 );
+objectScene.add( light );
 
 function animate() {
 	requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     // required if controls.enableDamping or controls.autoRotate are set to true
 	controls.update();
 
-	renderer.render( scene, camera );
+	renderer.render( objectScene, camera );
 }
 animate();
