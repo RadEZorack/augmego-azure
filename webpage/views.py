9a7a9372c85fb844be134ac72a961ea12c1fb815
Travis Miller <radezorack@gmail.com>
2023-11-05 16:53:12 -0500

    Adding rest framework to try to solve image of webpages

--- webpage/views.py
+++ webpage/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
-from django.core import serializers
+from rest_framework import serializers
 
 from webpage.models import WebPage
+
+class WebPageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+    class Meta:
+        model = WebPage
+        fields = '__all__'
 
 def list_webpages(request):
     webpages = WebPage.objects.all()
 
-    data = serializers.serialize('json', webpages)
+    serializer = WebPageSerializer(webpages, many=True, context={"request":request})
 
-    return HttpResponse(data, content_type='application/json')
+    return HttpResponse(serializer.data, content_type='application/json')
