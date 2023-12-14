db39fe6691bdbbb4285aae36d215364f576ea3c0
Travis Miller <radezorack@gmail.com>
2023-12-14 14:22:13 -0500

    updates to size of texture bar

--- static/main/initTexturePanel.js
+++ static/main/initTexturePanel.js
@@ -6,10 +6,10 @@ $.ajax({
     success: function(resp) {
         // console.log(resp);
         for(const i in resp){
-            const space = (i * 10).toString();
+            const space = (i * 5).toString();
             const fields = resp[i]
             const img = $("#texturePanel").append(
-                `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="position: absolute; border: solid 2px red; left:${space}vw" src="${fields["image_url"]}" alt="Toggle block place on" width="${100/8}%" height="100%">`
+                `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="position: absolute; border: solid 2px red; left:${space}vw" src="${fields["image_url"]}" alt="Toggle block place on" width="${100/18}%" height="100%">`
             )
         }
         initToggleMouseOption();
