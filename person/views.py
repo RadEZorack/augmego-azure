7a222b54c31a729d6f21a700f99bfaa56844878c
Travis Miller <radezorack@gmail.com>
2023-12-21 09:56:49 -0500

    added xframe_options_sameorigin to donation wall

--- person/views.py
+++ person/views.py
@@ -2,10 +2,12 @@ from decimal import Decimal
 from django.shortcuts import render
 from django.db.models import Sum, Value, Max
 from django.db.models.functions import Coalesce
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 from person.models import Person
 
 # Create your views here.
+@xframe_options_sameorigin
 def profile_wall(request):
     people = Person.objects.filter(user__is_superuser=False).annotate(total_payments=Coalesce(Sum("payment__amount"), Value(Decimal(0.00)))).annotate(most_recent_payment_date=Max('payment__created_at')).order_by("-total_payments")
 
