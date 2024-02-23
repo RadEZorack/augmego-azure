0f69025eeaf3a4be658f36cda5c9385d1e70367b
Travis Miller <radezorack@gmail.com>
2024-02-23 11:14:30 -0500

    Added a self cam so that the ipad can ask for permissions before failing in game.

---
+++ static/main/initSelfCam.js
@@ -0,0 +1,79 @@
+// navigator.getUserMedia(
+//  { video: true, audio: false },
+//  async stream => {
+
+//     const localVideo = document.getElementById("local-video");
+//      localVideo.onloadedmetadata = function(e) {
+//         localVideo.play();
+//       };
+//      if (localVideo) {
+//        localVideo.srcObject = stream;
+//      }
+//  },
+//  error => {
+//    console.warn(error.message);
+//  })
+
+function initSelfCam(){
+    let videoCount = 0;
+  
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+      navigator.mediaDevices = {};
+    }
+  
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function(constraints) {
+  
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+  
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function(resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      }
+    }
+  
+    navigator.mediaDevices.enumerateDevices()
+      .then(function(devices) {
+        devices.forEach(function(device) {
+          // console.log(device.kind + ": " + device.label +
+                      // " id = " + device.deviceId);
+          if(device.kind == "videoinput"){
+            console.log("trying")
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+              .then(function(stream) {
+                if (videoCount >= 1){
+                  return null;
+                }
+                videoCount += 1;
+                const localVideo = document.getElementById("local-video-"+videoCount.toString());
+                // console.log(localVideo);
+                localVideo.onloadedmetadata = function(e) {
+                    localVideo.play();
+                };
+                if (localVideo) {
+                  localVideo.srcObject = stream;
+                }
+              }).catch(function(err) {
+                console.log(err.name + ": " + err.message);
+              })
+          }
+        });
+      })
+      .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+      });
+  }
+  // 
+  initSelfCam();
--- static/main/initTexturePanel.js
+++ static/main/initTexturePanel.js
@@ -1,29 +1,42 @@
 import { initToggleMouseOption } from '../main/mouseClicks.js';
 import { initObjects } from '../main/initObjects.js';
 
-$.ajax({
-    url: textureLoadURL,
-    type: 'GET',
-    success: function(resp) {
-        // console.log(resp);
-        for(const i in resp){
-            const space = (i * 5).toString();
-            const fields = resp[i]
-            const img = $("#texturePanel").append(
-                `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="position: absolute; border: solid 2px red; left:${space}vw" src="${fields["image_url"]}" alt="Toggle block place on" width="${100/18}%" height="100%">`
-            )
-            
-            if(fields['name'] == "Grass"){
-                grassTexture = fields["image_url"]
-            }else if(fields['name'] == "Dirt"){
-                dirtTexture = fields["image_url"];
-            }else if(fields['name'] == "Bark"){
-                barkTexture = fields["image_url"];
-            }else if(fields['name'] == "Pine Tree Leaves"){
-                pineTreeLeavesTexture = fields["image_url"];
-            }
-        }
-        initToggleMouseOption();
-        initObjects();
+function textureLoad(){
+    if (stopAnimate == true){
+        // We need the player to interact with the page before things will work correctly.
+        // So we loop until we get the flag to start, which can be found on the main page.
+        const stopAnimateXHR = setTimeout(function(){
+            textureLoad()
+        }, 1000)
+        return null
     }
-})
+
+    $.ajax({
+        url: textureLoadURL,
+        type: 'GET',
+        success: function(resp) {
+            // console.log(resp);
+            for(const i in resp){
+                const space = (i * 5).toString();
+                const fields = resp[i]
+                const img = $("#texturePanel").append(
+                    `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="position: absolute; border: solid 2px red; left:${space}vw" src="${fields["image_url"]}" alt="Toggle block place on" width="${100/18}%" height="100%">`
+                )
+                
+                if(fields['name'] == "Grass"){
+                    grassTexture = fields["image_url"]
+                }else if(fields['name'] == "Dirt"){
+                    dirtTexture = fields["image_url"];
+                }else if(fields['name'] == "Bark"){
+                    barkTexture = fields["image_url"];
+                }else if(fields['name'] == "Pine Tree Leaves"){
+                    pineTreeLeavesTexture = fields["image_url"];
+                }
+            }
+            initToggleMouseOption();
+            initObjects();
+        }
+    })
+}
+
+textureLoad();
