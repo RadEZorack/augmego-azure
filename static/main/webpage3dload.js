ec12dd5610ad28e3dc8c92265adf1cdd02b37865
Travis Miller <radezorack@gmail.com>
2024-03-19 21:25:53 -0400

    loading chunk zones

--- static/main/webpage3dload.js
+++ static/main/webpage3dload.js
@@ -1,5 +1,6 @@
 import * as THREE from '../three/three.module.js';
 import { create3dPage } from '../main/webpage3d.js';
+import { playerWrapper } from '../main/player.js';
 
 // Example
 // create3dPage(
@@ -34,96 +35,123 @@ $.ajax({
     }
 })
 
-$.ajax({
-    url: chunkInfoURL,
-    type: 'GET',
-    data: {
-        csrfmiddlewaretoken: csrfmiddlewaretoken,
-        x: 0,
-        y: 0,
-        z: 0
-      },
-    success: function(resp) {
-        console.log(resp);
-        create3dPage(
-            1000,
-            10000,
-            0.001,
-            new THREE.Vector3(0+4.5, 0+4.5, 0-0.5),
-            new THREE.Vector3(0, 0, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            10000,
-            1000,
-            0.001,
-            new THREE.Vector3(0+4.5, 0+4.5, 0-0.5),
-            new THREE.Vector3(0, 0, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            1000,
-            10000,
-            0.001,
-            new THREE.Vector3(0+4.5, 0+4.5, 0+10-0.5),
-            new THREE.Vector3(0, 0, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            10000,
-            1000,
-            0.001,
-            new THREE.Vector3(0+4.5, 0+4.5, 0+10-0.5),
-            new THREE.Vector3(0, 0, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            1000,
-            10000,
-            0.001,
-            new THREE.Vector3(0+10-0.5, 0+4.5, 0+4.5),
-            new THREE.Vector3(0, Math.PI/2, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            10000,
-            1000,
-            0.001,
-            new THREE.Vector3(0+10-0.5, 0+4.5, 0+4.5),
-            new THREE.Vector3(0, Math.PI/2, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            1000,
-            10000,
-            0.001,
-            new THREE.Vector3(0-0.5, 0+4.5, 0+4.5),
-            new THREE.Vector3(0, Math.PI/2, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
-        create3dPage(
-            10000,
-            1000,
-            0.001,
-            new THREE.Vector3(0-0.5, 0+4.5, 0+4.5),
-            new THREE.Vector3(0, Math.PI/2, 0),
-            "",
-            '<div style="width: 100%; height: 100%; background: '+resp+'">',
-            ""
-        )
+
+function drawChunkBounds(chunkPosition){
+    let thisChunkPosition = new THREE.Vector3(0.0, 0.0, 0.0);
+    // console.log(playerWrapper);
+    if (playerWrapper != undefined){
+        thisChunkPosition = new THREE.Vector3(
+            Math.floor(playerWrapper.position.x/10)*10,
+            Math.floor(playerWrapper.position.y/10)*10,
+            Math.floor(playerWrapper.position.z/10)*10
+        );
+
+        const x = thisChunkPosition.x;
+        const y = thisChunkPosition.y;
+        const z = thisChunkPosition.z;
+        
+        // console.log(chunkPosition, thisChunkPosition)
+        if (chunkPosition == undefined || chunkPosition.x != x || chunkPosition.y != y || chunkPosition.z != z){ 
+            
+            $.ajax({
+                url: chunkInfoURL,
+                type: 'GET',
+                data: {
+                    csrfmiddlewaretoken: csrfmiddlewaretoken,
+                    x: Math.floor(x/10),
+                    y: Math.floor(y/10),
+                    z: Math.floor(z/10)
+                },
+                success: function(resp) {
+                    console.log(resp);
+                    // returns { plane: plane, cssObject: cssObject, scale: s }
+                    create3dPage(
+                        1000,
+                        10000,
+                        0.001,
+                        new THREE.Vector3(x+4.5, y+4.5, z-0.5),
+                        new THREE.Vector3(0, 0, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        10000,
+                        1000,
+                        0.001,
+                        new THREE.Vector3(x+4.5, y+4.5, z-0.5),
+                        new THREE.Vector3(0, 0, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        1000,
+                        10000,
+                        0.001,
+                        new THREE.Vector3(x+4.5, y+4.5, z+10-0.5),
+                        new THREE.Vector3(0, 0, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        10000,
+                        1000,
+                        0.001,
+                        new THREE.Vector3(x+4.5, y+4.5, z+10-0.5),
+                        new THREE.Vector3(0, 0, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        1000,
+                        10000,
+                        0.001,
+                        new THREE.Vector3(x+10-0.5, y+4.5, z+4.5),
+                        new THREE.Vector3(0, Math.PI/2, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        10000,
+                        1000,
+                        0.001,
+                        new THREE.Vector3(x+10-0.5, y+4.5, z+4.5),
+                        new THREE.Vector3(0, Math.PI/2, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        1000,
+                        10000,
+                        0.001,
+                        new THREE.Vector3(x-0.5, y+4.5, z+4.5),
+                        new THREE.Vector3(0, Math.PI/2, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                    create3dPage(
+                        10000,
+                        1000,
+                        0.001,
+                        new THREE.Vector3(x-0.5, y+4.5, z+4.5),
+                        new THREE.Vector3(0, Math.PI/2, 0),
+                        "",
+                        '<div style="width: 100%; height: 100%; background: '+resp+'">',
+                        ""
+                    )
+                }
+            })
+        }
     }
-})
+    setTimeout(function(){
+        drawChunkBounds(thisChunkPosition)
+    }, 1000)
+}
+
+drawChunkBounds(undefined)
