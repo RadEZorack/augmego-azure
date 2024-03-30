3d0a25fffdbcd7ab91a192008bad476fe3239072
Travis Miller <radezorack@gmail.com>
2024-03-30 17:52:37 -0400

    small bug fix with players being transparent

--- static/main/entity.js
+++ static/main/entity.js
@@ -82,7 +82,7 @@ export function update_entity(entity_data){
                 gltf.scene.traverse((node) => {
                     if (node.isMesh && node.material) {
                         node.material.transparent = true;
-                        node.material.opacity = 0.5; // Adjust as needed, ranging from 0.0 (fully transparent) to 1.0 (fully opaque)
+                        // node.material.opacity = 0.5; // Adjust as needed, ranging from 0.0 (fully transparent) to 1.0 (fully opaque)
                     }
                 });
                 objectScene.add( gltf.scene );
