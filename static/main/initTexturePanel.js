3a074bdfb5cc60f660cedac8754d2211c0549c64
Travis Miller <radezorack@gmail.com>
2024-04-20 22:14:28 -0400

    blocks panel tiled

--- static/main/initTexturePanel.js
+++ static/main/initTexturePanel.js
@@ -15,13 +15,36 @@ function textureLoad(){
         url: textureLoadURL,
         type: 'GET',
         success: function(resp) {
+            const container = document.getElementById('texturePanel');
+
+            let row = document.createElement('div');
+            row.className = 'row';
+
+            resp.forEach((image, index) => {
+                console.log(index, resp.length)
+                const col = document.createElement('div');
+                col.className = 'col-3 mb-3'; // added mb-3 for some bottom margin
+
+                const img = `<img width="64px" height="64px" class="toggleMouseOption img-fluid" data-type="create" data-material="${image['name']}" style="border: solid 2px red;" src="${image["image_url"]}" alt="Toggle block place on">`
+
+                col.innerHTML = img;
+                row.appendChild(col);
+
+                // Every 3 images or if it's the last image, append the row to the container and create a new row
+                if ((index + 1) % 4 === 0 || index + 1 === resp.length) {
+                    console.log("hit")
+                container.appendChild(row);
+                row = document.createElement('div');
+                row.className = 'row';
+                }
+            });
             // console.log(resp);
             for(const i in resp){
                 // const space = (i * 5).toString();
                 const fields = resp[i]
-                const img = $("#texturePanel").append(
-                    `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="border: solid 2px red;" src="${fields["image_url"]}" alt="Toggle block place on"></br>`
-                )
+                // const img = $("#texturePanel").append(
+                //     `<img class="toggleMouseOption" data-type="create" data-material="${fields['name']}" style="border: solid 2px red;" src="${fields["image_url"]}" alt="Toggle block place on"></br>`
+                // )
                 
                 if(fields['name'] == "Grass"){
                     grassTexture = fields["image_url"]
