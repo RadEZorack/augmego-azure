3aa11d08a31b4f66f472e7328b32146197c920e5
Travis Miller <radezorack@gmail.com>
2024-05-18 18:06:07 -0400

    updated asgi so that daphne could run

--- quickstartproject/asgi.py
+++ quickstartproject/asgi.py
@@ -13,14 +13,20 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import django
 
-from chat.routing import websocket_urlpatterns
-from game.routing import websocket_urlpatterns_game
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickstartproject.settings')
+
+# Ensure Django setup is called before accessing applications
+django.setup()
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
+from game.routing import websocket_urlpatterns_game
 
 # import chat.routing
 
