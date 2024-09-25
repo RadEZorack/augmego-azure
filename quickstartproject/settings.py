913de26d30602896502f41b15eaee2489e886902
Travis Miller <radezorack@gmail.com>
2024-09-25 09:57:38 -0400

    hard coding ALLOWED_HOSTS

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -36,13 +36,13 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 import socket
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',"167.99.179.219 127.0.0.1 localhost augmego-django augmego.ngrok.io 369de4cfa06e-7199118840071997290.ngrok-free.app").split(" ")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',"augmego.com www.augmego.com 167.99.179.219 127.0.0.1 localhost augmego-django augmego.ngrok.io 369de4cfa06e-7199118840071997290.ngrok-free.app").split(" ")
 ALLOWED_HOSTS += [ip_address,]
 ALLOWED_HOSTS += [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS',"https://localhost https://augmego-django https://augmego.ngrok.io https://369de4cfa06e-7199118840071997290.ngrok-free.app").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS',"https://augmego.com https://www.augmego.com https://localhost https://augmego-django https://augmego.ngrok.io https://369de4cfa06e-7199118840071997290.ngrok-free.app").split(" ")
 CSRF_TRUSTED_ORIGINS += ["http://"+ip_address,"https://"+ip_address]
 
 # Application definition
