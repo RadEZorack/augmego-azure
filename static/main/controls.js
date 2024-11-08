74c9ab60b4509195e462a409bd9efe4fa9a318cd
Travis Miller <radezorack@gmail.com>
2024-11-08 17:40:13 -0500

    small update

--- static/main/controls.js
+++ static/main/controls.js
@@ -340,12 +340,10 @@ export function initControls() {
   /////////////////// RIGHT JOYSTICK ////////////////
   let rightJoystickTouch = undefined;
   function rightJoystickTouchstart(event) {
-    if (isOrientationActive == false){
-      getOrientation();
-      isOrientationActive = true;
-    }
+    
     event.preventDefault();
     event.stopPropagation();
+    
     // If there's exactly one finger inside this element
     if (event.targetTouches && event.targetTouches.length == 1) {
       rightJoystickTouch = event.targetTouches[0];
@@ -377,6 +375,11 @@ export function initControls() {
     );
     // update_sigmoids();
     updateJoystickSymbols();
+
+    if (isOrientationActive == false){
+      getOrientation();
+      isOrientationActive = true;
+    }
   }
 
   function rightJoystickTouchmove(event) {
